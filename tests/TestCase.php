307e9041f5e7cbd9d1ee5995caf369107f6d7f3b
dearvn <dearvn@users.noreply.github.com>
2023-09-05 16:51:16 +0000

    Fix styling

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -4,6 +4,7 @@ namespace Dearvn\FilamentImpersonate\Tests;
 
 use BladeUI\Heroicons\BladeHeroiconsServiceProvider;
 use BladeUI\Icons\BladeIconsServiceProvider;
+use Dearvn\FilamentImpersonate\FilamentImpersonateServiceProvider;
 use Filament\Actions\ActionsServiceProvider;
 use Filament\FilamentServiceProvider;
 use Filament\Forms\FormsServiceProvider;
@@ -18,7 +19,6 @@ use Illuminate\Database\Eloquent\Factories\Factory;
 use Livewire\LivewireServiceProvider;
 use Orchestra\Testbench\TestCase as Orchestra;
 use RyanChandler\BladeCaptureDirective\BladeCaptureDirectiveServiceProvider;
-use Dearvn\FilamentImpersonate\FilamentImpersonateServiceProvider;
 
 class TestCase extends Orchestra
 {
